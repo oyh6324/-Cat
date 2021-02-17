599cb005f3417ecb8ce1a0d676a9139e33fe50c9
oyh6415 <dudgus6324@naver.com>
2021-02-17 20:36:33 +0900

    수정
    * 무기고 배경
    * 이용권 충전 수정

--- Assets/Script/0. DemoSC/FreeTicketReset.cs
+++ Assets/Script/0. DemoSC/FreeTicketReset.cs
@@ -5,19 +5,32 @@ using System;
 
 public class FreeTicketReset : MonoBehaviour
 {
+    private void Start()
+    {
+        if (DemoDataManager.moneyItemList[3].count <= 0)
+        {
+            if (System.DateTime.Now.ToString("dd") != PlayerPrefs.GetString("이용권용 종료 시간")) //게임 들어왔을 때 날짜가 다르면 충전
+                DemoDataManager.moneyItemList[3].count++;
+        }
+        if (DemoDataManager.moneyItemList[5].count <= 0)
+        {
+            if (System.DateTime.Now.ToString("dd") != PlayerPrefs.GetString("이용권용 종료 시간"))
+                DemoDataManager.moneyItemList[5].count++;
+        }
+    }
     private void Update()
     {
         if (DemoDataManager.moneyItemList[3].count <= 0) //멸치 의상 쿠폰
         {
-            if (System.DateTime.Now.ToString("hh") == "00" || System.DateTime.Now.ToString("dd")!=PlayerPrefs.GetString("이용권용 종료 시간"))
-            { //게임 도중 00시가 되거나 게임에 들어왔을 때 날짜가 다르면 충전
+            if (System.DateTime.Now.ToString("hhss") == "0000") //게임 도중 오전 12시가 되면 충전
+            {
                 DemoDataManager.moneyItemList[3].count++;
             }
         }
         if (DemoDataManager.moneyItemList[5].count <= 0)
         {
-            if (System.DateTime.Now.ToString("hh") == "00" || System.DateTime.Now.ToString("dd") != PlayerPrefs.GetString("이용권용 종료 시간"))
-            { //게임 도중 00시가 되거나 게임에 들어왔을 때 날짜가 다르면 충전
+            if (System.DateTime.Now.ToString("hhss") == "0000")
+            { 
                 DemoDataManager.moneyItemList[5].count++;
             }
         }
