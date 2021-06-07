37b2909742b1ed68d757eb1fe1d33710479ef817
oyh6415 <dudgus6324@naver.com>
2021-06-07 16:59:15 +0900

    스테이지 매니저&레벨 업 메시지
    * 스테이지 클리어/미클리어 후속처리 완료
    * 레벨 업 메시지 외관 수정

--- Assets/Script/0. DemoSC/Lobby/DemoLevelManager.cs
+++ Assets/Script/0. DemoSC/Lobby/DemoLevelManager.cs
@@ -10,16 +10,23 @@ public class DemoLevelManager : MonoBehaviour
     public Image messageImg;
     public Image levelupMessageImg;
     public Text titleTx;
-    public Text rewardTx;
     public Text statTx;
-    //경험치 바
-    public Slider expSliberBar;
-    public Text expSliderBarTx;
-    public Text levelTx;
-    public Text playerNameTx;
+
+    public Image rewardImg;
+    public Text rewardTx;
+    public Image ticket;
+
+    public Sprite anchovy;
+    public Sprite pearl;
+    public Sprite anchovyTicket;
+    public Sprite pearlTicket;
+
     //오디오
     public AudioSource soundEffectAS;
     public AudioClip buttonClickClip;
+
+    private Sprite reward1;
+    private Sprite reward2;
     void OnEnable()
     {
         if (DemoDataManager.characterDatasList[0].exp >= DemoDataManager.characterDatasList[0].totalexp)
@@ -42,7 +49,6 @@ public class DemoLevelManager : MonoBehaviour
         int reward1Count = 0, reward2Count = 0;
         int preHp=DemoDataManager.characterDatasList[0].hp;
         decimal crip;
-        string rewardName1="", rewardName2="";
 
         DemoDataManager.characterDatasList[0].level += 1; //레벨 올리기
         level = DemoDataManager.characterDatasList[0].level;
@@ -59,65 +65,65 @@ public class DemoLevelManager : MonoBehaviour
         agi = DemoDataManager.characterDatasList[0].agi;
         crip = DemoDataManager.characterDatasList[0].crip;
         hp = DemoDataManager.characterDatasList[0].hp;
+
         //보상
         if (level < 5) //1~4
         {
-            rewardName1 = "멸치"; rewardName2 = "진주";
+            reward1 = anchovy; reward2 = pearl;
             reward1Count = level * 500; reward2Count = (level - 1) * 3;
             DemoDataManager.moneyItemList[0].count += reward1Count;
             DemoDataManager.moneyItemList[1].count += reward2Count;
         }
         else if (level == 5) //5
         {
-            rewardName1 = "멸치 보석함 무료이용권(의상)"; rewardName2 = "";
+            reward1 = anchovyTicket; reward2 = null;
             reward1Count = 1;
             DemoDataManager.moneyItemList[3].count += reward1Count;
         }
         else if (level > 5 && level < 10) //6~9
         {
-            rewardName1 = "멸치"; rewardName2 = "진주";
+            reward1 = anchovy; reward2 = pearl;
             reward1Count = (level - 1) * 500; reward2Count = (level - 4) * 5;
             DemoDataManager.moneyItemList[0].count += reward1Count;
             DemoDataManager.moneyItemList[1].count += reward2Count;
         }
         else if (level == 10) //10
         {
-            rewardName1 = "진주 보석함 무료이용권(의상)"; rewardName2 = "";
+            reward1 = pearlTicket; reward2 = null;
             reward1Count = 1;
             DemoDataManager.moneyItemList[4].count += reward1Count;
         }
         else if (level > 10 && level < 15) //11~14
         {
-            rewardName1 = "멸치"; rewardName2 = "진주";
+            reward1 = anchovy; reward2 = pearl;
             reward1Count = (level - 2) * 500; reward2Count = 19 + (level - 10) * 7;
             DemoDataManager.moneyItemList[0].count += reward1Count;
             DemoDataManager.moneyItemList[1].count += reward2Count;
         }
         else if (level == 15) //15
         {
-            rewardName1 = "멸치 보석함 무료이용권(의상)"; rewardName2 = "진주 보석함 무료이용권(의상)";
+            reward1 = anchovyTicket; reward2 = pearlTicket;
             reward1Count = 1; reward2Count = 1;
             DemoDataManager.moneyItemList[3].count += reward1Count;
             DemoDataManager.moneyItemList[4].count += reward2Count;
         }
         else if (level > 15 && level < 20) //16~19
         {
-            rewardName1 = "멸치"; rewardName2 = "진주";
+            reward1 = anchovy; reward2 = pearl;
             reward1Count = (level - 3) * 500; reward2Count = 39 + (level - 15) * 9;
             DemoDataManager.moneyItemList[0].count += reward1Count;
             DemoDataManager.moneyItemList[1].count += reward2Count;
         }
         else if (level == 20) //20
         {
-            rewardName1 = "멸치"; rewardName2 = "진주";
-            reward1Count = 10000; reward2Count = 100;
-            DemoDataManager.moneyItemList[0].count += 10000;
-            DemoDataManager.moneyItemList[1].count += 100;
-            DemoDataManager.moneyItemList[4].count += 1;
+            reward1 = anchovy; reward2 = pearl;
+            reward1Count = 15000; reward2Count = 150;
+            DemoDataManager.moneyItemList[0].count += 15000;
+            DemoDataManager.moneyItemList[1].count += 150;
         }
-        if (rewardName1 == "멸치") //업적 연동
+        if (reward1 == anchovy) //업적 연동
             DemoDataManager.achievementDataList[0].progressvalue += reward1Count;
-        if(rewardName2=="진주")
+        if(reward2 == pearl)
             DemoDataManager.achievementDataList[1].progressvalue += reward2Count;
 
         //all 스탯 데이터 초기화
@@ -127,11 +133,35 @@ public class DemoLevelManager : MonoBehaviour
         DemoDataManager.characterDatasList[0].allcrip = crip + DemoDataManager.characterDatasList[0].itemcrip + DemoDataManager.characterDatasList[0].setcrip;
 
         titleTx.text = "Lv." + level;
-        rewardTx.text = rewardName1 + " +" + reward1Count;
-        if (rewardName2 != "")
-            rewardTx.text += " " + rewardName2 + " +" + reward2Count;
-        if (level == 20)
-            rewardTx.text += "\n 진주 보석함 무료이용권(의상) +1";
+
+        if(reward1==anchovy|| reward1==pearl) //보상1
+        {
+            rewardImg.gameObject.SetActive(true);
+            rewardImg.sprite = reward1;
+            rewardTx.text = "X" + reward1Count;
+        }
+        else
+        {
+            ticket.gameObject.SetActive(true);
+            ticket.sprite = reward1;
+        }
+
+        if (reward2 != null) //보상2
+        {
+            if (reward1 == anchovy || reward1 == pearl) 
+            {
+                Image rewardImg2 = Instantiate(rewardImg, rewardImg.transform.parent);
+                rewardImg2.sprite = reward2;
+                rewardImg2.GetComponentInChildren<Text>().text = "X" + reward2Count;
+            }
+            else
+            {
+                Image rewardImg2 = Instantiate(ticket, ticket.transform.parent);
+                rewardImg2.sprite = reward2;
+                rewardImg2.GetComponentInChildren<Text>().text = "X" + reward2Count;
+            }
+        }
+
         statTx.text = preHp.ToString() + "→" + hp.ToString() + "\n" + (str - 3).ToString() + "→" + str.ToString() + "\n" + (def - 1).ToString() + "→" + def.ToString() +
             "\n" + (agi - 2).ToString() + "→" + agi.ToString() + "\n" + (crip - (decimal)0.1).ToString() + "→" + crip.ToString();
     }
